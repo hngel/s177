4091015a88eb2f7df2bfb7088ff6670070f57c24
hngel <41763813+hngel@users.noreply.github.com>
2018-11-18 17:42:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,64 +1,76 @@
-const Discord = require('discord.js');//BY.MR.client
-const client = new Discord.Client();//BY.MR.client
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
 client.on('ready', () => {
-  console.log(`MR.client`);  //BY.MR.client
-//BY.MR.client
+   console.log(`----------------`);
+   console.log(`Script By :hngel `);
+   console.log(`----------------`);
+   console.log(`Loadinng`);
+   console.log(`Loadinng.`);
+   console.log(`Loadinng..`);
+   console.log(`Loadinng...`);
+   console.log(`This Bots Online ' `);
+   console.log(`----------------`);
 });
+
+
 //BY.MR.client
 client.on('message', message => {    //BY.MR.client
-const adminprefix = "$";  
-const devs = ["471013646647164938","269031102340005888"];
-  var argresult = message.content.split(` `).slice(1).join(' ');  
-    if (!devs.includes(message.author.id)) return;  //BY.MR.client
-if (message.content.startsWith(adminprefix + 'p')) {  
-  client.user.setGame(argresult);//BY.MR.client
-const embed = new Discord.RichEmbed() 
-.setColor("#161d99")
-    .setDescription(`**${argresult} تم تغيير بلاينق إلى 💙**`)//BY.MR.client
-  message.channel.sendEmbed(embed);
-message.react("✔")
-      } else     //BY.MR.client
-if (message.content.startsWith(adminprefix + 't')) {//BY.MR.client
-  client.user.setGame(argresult, "https://www.twitch.tv/client2667");//BY.MR.client
-const embed = new Discord.RichEmbed() 
- .setFooter(`Ⓕ -.9#3565`)
-.setColor("#161d99")
-   .setDescription(`**💜تم تغيير تويتش إلى  ${argresult}**`);//BY.MR.client
-  message.channel.sendEmbed(embed);
-message.react("✔")
- } else     //BY.MR.client
-if (message.content.startsWith(adminprefix + 'l')) {//BY.MR.client
-  client.user.setActivity(argresult,{type: 'listening'});//BY.MR.client
-const embed = new Discord.RichEmbed() 
-.setColor("#161d99")
-    .setDescription(`**🖤تم تغيير ليستنق إلى ${argresult}**`);//BY.MR.client
-  message.channel.sendEmbed(embed);
-message.react("✔")
- } else     //BY.MR.client
-if (message.content.startsWith(adminprefix + 'w')) {//BY.MR.client
-  client.user.setActivity(argresult,{type: 'WATCHING'});//BY.MR.client
-const embed = new Discord.RichEmbed() 
-.setColor("#161d99")
-   .setDescription(`**💛تم تغيير واتشنق إلى  ${argresult}**`);//BY.MR.client
-  message.channel.sendEmbed(embed);
-message.react("✔")
+    const adminprefix = "$";  
+    const devs = ['471013646647164938'];
+      var argresult = message.content.split(` `).slice(1).join(' ');  
+        if (!devs.includes(message.author.id)) return;  //BY.MR.client
+    if (message.content.startsWith(adminprefix + 'p')) {  
+      client.user.setGame(argresult);//BY.MR.client
+    const embed = new Discord.RichEmbed() 
+    .setColor("#161d99")
+        .setDescription(`**${argresult} تم تغيير بلاينق إلى 💙**`)//BY.MR.client
+      message.channel.sendEmbed(embed);
+    message.react("✔")
+          } else     //BY.MR.client
+    if (message.content.startsWith(adminprefix + 't')) {//BY.MR.client
+      client.user.setGame(argresult, "https://www.twitch.tv/hngel2667");//BY.MR.client
+    const embed = new Discord.RichEmbed() 
+     .setFooter(`Ⓕ -.9#3565`)
+    .setColor("#161d99")
+       .setDescription(`**💜تم تغيير تويتش إلى  ${argresult}**`);//BY.MR.client
+      message.channel.sendEmbed(embed);
+    message.react("✔")
+     } else     //BY.MR.client
+    if (message.content.startsWith(adminprefix + 'l')) {//BY.MR.client
+      client.user.setActivity(argresult,{type: 'listening'});//BY.MR.client
+    const embed = new Discord.RichEmbed() 
+    .setColor("#161d99")
+        .setDescription(`**🖤تم تغيير ليستنق إلى ${argresult}**`);//BY.MR.client
+      message.channel.sendEmbed(embed);
+    message.react("✔")
+     } else     //BY.MR.client
+    if (message.content.startsWith(adminprefix + 'w')) {//BY.MR.client
+      client.user.setActivity(argresult,{type: 'WATCHING'});//BY.MR.client
+    const embed = new Discord.RichEmbed() 
+    .setColor("#161d99")
+       .setDescription(`**💛تم تغيير واتشنق إلى  ${argresult}**`);//BY.MR.client
+      message.channel.sendEmbed(embed);
+    message.react("✔")
+    
+    } else     //BY.MR.client
+    if (message.content.startsWith('-')) {//BY.MR.client
+      
+    const embed = new Discord.RichEmbed() 
+    .setColor("#161d99")
+       .setDescription(`**${argresult}**`);//BY.MR.client
+      message.channel.sendEmbed(embed);
+    message.delete();
+    
+    
+    //BY.MR.client
+    
+    
+    //BY.MR.client
+    }
+    //BY.MR.client
+    
+    });//BY.MR.client
 
-} else     //BY.MR.client
-if (message.content.startsWith('-')) {//BY.MR.client
-  
-const embed = new Discord.RichEmbed() 
-.setColor("#161d99")
-   .setDescription(`**${argresult}**`);//BY.MR.client
-  message.channel.sendEmbed(embed);
-message.delete();
 
-
-//BY.MR.client
-
-
-//BY.MR.client
-}
-//BY.MR.client
-
-});//BY.MR.client
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.TOKEN);// لا تغير فيها شيء
