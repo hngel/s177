5a521957396c2c2feb29b742db5745de17383c16
hngel <41763813+hngel@users.noreply.github.com>
2018-11-18 18:30:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,62 +1,26 @@
-const Discord = require('discord.js');//BY.MR.client
-const client = new Discord.Client();//BY.MR.client
+const Discord = require('discord.js');
+const client = new Discord.Client();
 client.on('ready', () => {
-  console.log(`MR.87`);  //BY.MR.client
-});
-client.on('message', message => {    //BY.MR.client
-const adminprefix = "$";  
-const devs = ['471013646647164938'];
-  var argresult = message.content.split(` `).slice(1).join(' ');  
-    if (!devs.includes(message.author.id)) return;  //BY.MR.client
-if (message.content.startsWith(adminprefix + 'p')) {  
-  client.user.setGame(argresult);//BY.MR.client
+  console.log(` streem on`);  
+});//---------------------------------------------------------=fs
+client.on('message', message => {
+    var prefix = "$"; 
+      if (!message.content.startsWith(prefix)) return;
+      var args = message.content.split(' ').slice(1);
+      var argresult = args.join(' ');
+if (message.content.startsWith(prefix)) {
+  if (message.author.id !== '471013646647164938') return
+client.user.setGame(argresult, "https://www.twitch.tv/hngel2667"); 
 const embed = new Discord.RichEmbed() 
-.setColor("#161d99")
-    .setDescription(`**${argresult} تم تغيير بلاينق إلى 💙**`)//BY.MR.client
-  message.channel.sendEmbed(embed);
-message.react("✔")
-      } else     //BY.MR.client
-if (message.content.startsWith(adminprefix + 't')) {//BY.MR.client
-  client.user.setGame(argresult, "https://www.twitch.tv/client2667");//BY.MR.client
-const embed = new Discord.RichEmbed() 
- .setFooter(`Ⓕ -.9#3565`)
-.setColor("#161d99")
-   .setDescription(`**💜تم تغيير تويتش إلى  ${argresult}**`);//BY.MR.client
-  message.channel.sendEmbed(embed);
-message.react("✔")
- } else     //BY.MR.client
-if (message.content.startsWith(adminprefix + 'l')) {//BY.MR.client
-  client.user.setActivity(argresult,{type: 'listening'});//BY.MR.client
-const embed = new Discord.RichEmbed() 
-.setColor("#161d99")
-    .setDescription(`**🖤تم تغيير ليستنق إلى ${argresult}**`);//BY.MR.client
-  message.channel.sendEmbed(embed);
-message.react("✔")
- } else     //BY.MR.client
-if (message.content.startsWith(adminprefix + 'w')) {//BY.MR.client
-  client.user.setActivity(argresult,{type: 'WATCHING'});//BY.MR.client
-const embed = new Discord.RichEmbed() 
-.setColor("#161d99")
-   .setDescription(`**💛تم تغيير واتشنق إلى  ${argresult}**`);//BY.MR.client
-  message.channel.sendEmbed(embed);
-message.react("✔")
-
-} else     //BY.MR.client
-if (message.content.startsWith('-')) {//BY.MR.client
-  
-const embed = new Discord.RichEmbed() 
-.setColor("#161d99")
-   .setDescription(`**${argresult}**`);//BY.MR.client
-  message.channel.sendEmbed(embed);
-message.delete();
-
-
-//BY.MR.client
-
-
-//BY.MR.client
+             .setColor("#010700")
+             .setThumbnail(message.author.avatarURL)
+            .setFooter(`Ⓕ -.9#3565`)
+             .addField("✔تـــــــم ")
+             .setDescription(`**${argresult}** تم تغيير تويتش إلى`)
+    message.channel.sendEmbed(embed);
+    
 }
-//BY.MR.client
+
 
 });//BY.MR.client
 client.login(process.env.TOKEN);
